5fd329f526d784ace2abc13788084190ad7f8a3c
Tal Lev-Ami <tal.levami@gmail.com>
2013-05-19 11:45:32 +0300

    Support discard_original_filename

--- src/js/lib/cloudinary.triggerio.js
+++ src/js/lib/cloudinary.triggerio.js
@@ -424,6 +424,7 @@
       eager: build_eager(options.eager),
       headers: build_custom_headers(options.headers),
       use_filename: options.use_filename,
+      discard_original_filename: options.discard_original_filename,
       notification_url: options.notification_url,
       eager_notification_url: options.eager_notification_url,
       eager_async: options.eager_async,
